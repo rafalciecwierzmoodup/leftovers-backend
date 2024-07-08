9285cb16ff619fdb986cd4e900972d39faf65f53
Rafał Ciećwierz <r.ciecwierz@moodup.team>
2024-07-08 10:57:02 +0200

    Add: mockedUser function

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { UsersRepository } from './repositories/user.repository';
+import { mockedUser } from '../../test/mocks/mockUser';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -28,28 +29,13 @@ describe('UsersService', () => {
   });
 
   it('findAll should call and return getUsers from repository', async () => {
-    const mockedDate = new Date();
-    const mockUsers = [
-      {
-        id: '5e407bf7-e682-410f-b364-cfdf2bb516e9',
-        email: 'Jesus_Lebsack51@gmail.com',
-        password: 'ImSLyqr8',
-        createdAt: mockedDate,
-        updatedAt: mockedDate,
-      },
-      {
-        id: '08da3955-cb4b-42b8-8817-130674684b98',
-        email: 'Raul.Glover5@gmail.com',
-        password: 'Lu6qhKt8',
-        createdAt: mockedDate,
-        updatedAt: mockedDate,
-      },
-    ];
+    const mockedUsers = [mockedUser(), mockedUser()];
+
     jest
       .spyOn(usersRepository, 'getUsers')
-      .mockReturnValue(Promise.resolve(mockUsers));
+      .mockReturnValue(Promise.resolve(mockedUsers));
 
-    expect(await service.findAll()).toBe(mockUsers);
+    expect(await service.findAll()).toBe(mockedUsers);
     expect(usersRepository.getUsers).toHaveBeenCalledTimes(1);
   });
 });
